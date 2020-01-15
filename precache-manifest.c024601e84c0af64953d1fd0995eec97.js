05ab0615a79d31abce40d04ab6fcd4880f60117c
Prachurjya Biswas <biswasprachurjya@gmail.com>
2020-01-15 22:33:07 +0530

    Updates

---
+++ precache-manifest.c024601e84c0af64953d1fd0995eec97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f83054d0b0b9be5d94dbf107e57fed",
+    "url": "/redditlistentothis/index.html"
+  },
+  {
+    "revision": "a4e795c2792348417add",
+    "url": "/redditlistentothis/static/css/main.09f7dd75.chunk.css"
+  },
+  {
+    "revision": "1910c8f6d35f118d6e1c",
+    "url": "/redditlistentothis/static/js/2.31a3e1a7.chunk.js"
+  },
+  {
+    "revision": "a4e795c2792348417add",
+    "url": "/redditlistentothis/static/js/main.555ebb66.chunk.js"
+  },
+  {
+    "revision": "a83a3d03664187bf61b9",
+    "url": "/redditlistentothis/static/js/runtime~main.48ee2a74.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FiddleINC/redditlistentothis/precache-manifest.26ccb7cb5dbcae8be0616710ef84f65d.js"
+  "/redditlistentothis/precache-manifest.c024601e84c0af64953d1fd0995eec97.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FiddleINC/redditlistentothis/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redditlistentothis/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
